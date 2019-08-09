02776cb3b861c189aa6daad9aebbb1d6f3903a16
Sergey Chertkov <sergeychertkov@iMac-Sergey.local>
2019-08-09 19:03:00 +0300

    webpack setup, step 2

---
+++ webpack.development.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+    entry: './js/index.js',
+    mode: 'development',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js', '.scss', '.otf'],
+        modules: [
+            __dirname,
+            path.resolve(__dirname, 'node_modules')
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            }
+        ]
+    }
+}
